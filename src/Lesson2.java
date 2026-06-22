6186b3e44d4a334d0e1ed2c76edf76407797a02b
gevorgyangarik27-star <gevorgyangarik27@gmail.com>
2026-06-22 14:53:02 +0400

    added homework 1

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,12 +1,10 @@
-public class Lesson1 {
-
+public class Lesson2 {
     public static void main(String[] args) {
-
-        for (int row = 1; row <= 5; row++) {
-            for (int col = 1; col <= row; col++) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
+}
