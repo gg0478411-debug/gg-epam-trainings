1b153ce7dde86013f363bc23bf9fa755751a8870
Ani Kovalenko <ani.kovalenko97@gmail.com>
2026-06-23 17:36:17 +0400

    Set homework 2

--- src/lesson1/Lesson1.java
+++ src/lesson1/Lesson1.java
@@ -1,3 +1,5 @@
+package lesson1;
+
 public class Lesson1 {
 
     public static void main(String[] args) {
@@ -10,3 +12,4 @@ public class Lesson1 {
         }
 
     }
+}
--- src/lesson1/Lesson2.java
+++ src/lesson1/Lesson2.java
@@ -1,3 +1,5 @@
+package lesson1;
+
 public class Lesson2 {
     public static void main(String[] args) {
         for (int i = 5; i >= 1; i--) {
